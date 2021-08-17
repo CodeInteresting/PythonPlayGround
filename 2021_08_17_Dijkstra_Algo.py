fdd1c220d73d624fe6b8209a479597ff0d0ea36e
Steven Shao <stevenshao2016@icloud.com>
2021-08-17 16:54:48 +0800

    Create 2021_08_17_Dijkstra_Algo.py

---
+++ 2021_08_17_Dijkstra_Algo.py
@@ -0,0 +1,49 @@
+# Dijkstra Algorithm
+
+graph = {}  # Dict
+graph["MusicBook"] = {"BlackDisc": 5, "Poster": 0}
+graph["BlackDisc"] = {"BassGuitar": 15, "Drum": 20}
+graph["Poster"] = {"BassGuitar": 30, "Drum": 35}
+graph["BassGuitar"] = {"Piano": 20}
+graph["Drum"] = {"Piano": 10}
+graph["Piano"] = {}
+
+costs = {}  # Dict
+infinity = float("inf")  # positive infinity
+costs["MusicBook"] = 0
+costs["BlackDisc"] = infinity
+costs["Poster"] = infinity
+costs["BassGuitar"] = infinity
+costs["Drum"] = infinity
+costs["Piano"] = infinity
+
+processed_nodes = set()
+
+parents = {}
+
+
+def find_lowest_cost_node():
+    lowestCost = infinity
+    nodeFound = None
+    for node, cost in costs.items():
+        if (node not in processed_nodes) and (cost < lowestCost):
+            lowestCost = cost
+            nodeFound = node
+    return nodeFound
+
+
+def dijkstra_algo():
+    lowest_cost_node = "MusicBook"  # hard code
+    while lowest_cost_node is not None:
+        neighbours = graph[lowest_cost_node]
+        for node, cost in neighbours.items():
+            newCost = costs[lowest_cost_node] + cost
+            if newCost < costs[node]:
+                costs[node] = newCost
+                parents[node] = lowest_cost_node
+        processed_nodes.add(lowest_cost_node)
+        lowest_cost_node = find_lowest_cost_node()
+
+
+dijkstra_algo()
+print(parents)
