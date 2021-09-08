18f8da5d195a9c77c6f42aa0eb3ef060d61669d4
Steven Shao <stevenshao2016@icloud.com>
2021-09-08 11:07:54 +0800

    Update Sum.py

--- Sum.py
+++ Sum.py
@@ -3,6 +3,7 @@ def Sum(num):
         return 0
     return num + Sum(num - 1)
 
+
 print(Sum(10))
 
 
@@ -11,19 +12,19 @@ def Sum2(num, agg):
         return agg
     return Sum2(num - 1, agg + num)
 
+
 print(Sum2(10, 0))
+
 
 # f(0) = 0
 # f(1) = 1
 # f(n) = f(n - 1) + f(n - 2)
-#
-
 def Fib(n):
     if n < 2:
         return n
-    N_minus_one = 1 # f(1)
-    N_minus_two = 0 # f(0)
-    N = 0 # (f(2))
+    N_minus_one = 1  # f(1)
+    N_minus_two = 0  # f(0)
+    N = 0  # (f(2))
 
     for i in range(2, n + 1):
         N = N_minus_one + N_minus_two
@@ -31,20 +32,23 @@ def Fib(n):
         N_minus_one = N
     return N
 
+
 def Fib2(n):
     if n < 2:
         return n
-    return Fib2(n - 1)  + Fib2(n - 2)
+    return Fib2(n - 1) + Fib2(n - 2)
+
 
 def Fib3(n, cache):
     if n < 2:
         return n
-    
+
     if n in cache:
         return cache[n]
-    else: 
+    else:
         cache[n] = Fib3(n - 2, cache) + Fib3(n - 1, cache)
         return cache[n]
+
 
 print(Fib(0))
 print(Fib(1))
@@ -55,5 +59,3 @@ print(Fib2(1))
 print(Fib2(11))
 
 print(Fib3(11, {}))
-
-    
